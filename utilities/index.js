45844ca1bb5f4f2ad2490afc7caedd8e63c0f584
Sarah Glenn <sar00@byui.edu>
2025-03-20 12:45:41 +0100

    adding select label

--- utilities/index.js
+++ utilities/index.js
@@ -87,7 +87,7 @@ Util.buildVehicleDetail = async function(data){
 Util.buildClassificationList = async function (classification_id = null) {
     let data = await invModel.getClassifications()
     let classificationList =
-      '<select name="classification_id" id="classificationList" required>'
+      '<label>Choose a classification</label><select name="classification_id" id="classificationList" required>'
     classificationList += "<option value=''>Choose a Classification</option>"
     data.rows.forEach((row) => {
       classificationList += `<option value="${row.classification_id}"`
